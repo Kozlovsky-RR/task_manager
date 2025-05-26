38894c3a48c7a6801362bfb430d83c8d56bb5f9d
Ruslan Kozlovsky <kenny32534@gmail.com>
2025-05-26 16:42:29 +0400

    add title, description, version

--- app/main.py
+++ app/main.py
@@ -7,7 +7,9 @@ from app.router.user_router import router as user_router
 from app.auth.jwt_auth import router as auth_router
 
 
-app = FastAPI()
+app = FastAPI(
+    title="Менеджер задач", description="Менеджер работы с задачами.", version="1"
+)
 
 
 app.include_router(auth_router)
